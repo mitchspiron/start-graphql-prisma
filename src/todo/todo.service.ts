2692e2cc4c987f8344902f22a1c3ef1c7acf22f3
mitchspiron <mitchspiron280@gmail.com>
2023-04-06 13:24:36 +0300

    first commit

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoInput } from './dto/create-todo.input';
+import { UpdateTodoInput } from './dto/update-todo.input';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class TodoService {
+  constructor(private prisma: PrismaService) {}
+  async create(createTodoInput: CreateTodoInput) {
+    return await this.prisma.todo.create({
+      data: {
+        title: createTodoInput.title,
+      },
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.todo.findMany();
+  }
+
+  async findOne(id: number) {
+    return await this.prisma.todo.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateTodoInput: UpdateTodoInput) {
+    return await this.prisma.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        title: updateTodoInput.title,
+      },
+    });
+  }
+
+  async remove(id: number) {
+    return await this.prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
